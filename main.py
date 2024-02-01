2b2650cf28643730842b2ad6790994a12fafe0c8
lucasRivera21 <lucasriverar2001@gmail.com>
2024-01-31 19:34:23 -0500

    feat: :card_file_box: connect database

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from sqlalchemy.orm import Session
+from db import engine, SessionLocal, Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
